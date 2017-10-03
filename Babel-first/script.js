0e06035ca3315fc55fbdbdedf46749c25fa9829f
ZBearLee <776010616@qq.com>
2017-10-03 21:46:23 +0800

    Update script.js

--- Babel-first/script.js
+++ Babel-first/script.js
@@ -1,10 +1,5 @@
-//let ani='animal';
-
-//Es6支持class关键字
-//类由属性，方法，构造函数
-//实例化时立即运行
 'use strict';
-class Chef {       //Chef 为类
+class Chef {       //Chef类
     constructor(food){     //constructor为构造函数
         this.food=food;    //初始化
         this.dish=[];
@@ -32,8 +27,8 @@ this.dish.push(dish);
 let chef=new Chef('毛豆炒肉');
 chef.cook();
 chef.food='炸鸡';
-chef.menu='炸虾球';  //.menu比.food更复杂
+chef.menu='炸虾球';
 chef.menu='披萨';      
-//chef.dish.push('肯德基');      使用menu会变得更私有
+//chef.dish.push('肯德基');   
 console.log(chef.menu);
 
