c75f847911efd3da47b66dbb334bd1d6b3bc4d8d
ZBearLee <776010616@qq.com>
2017-10-03 21:47:29 +0800

    Update build.js

--- Babel-first/build.js
+++ Babel-first/build.js
@@ -1,8 +1,3 @@
-//let ani='animal';
-
-//Es6支持class关键字
-//类由属性，方法，构造函数
-//实例化时立即运行
 'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -22,7 +17,6 @@ var Chef = function () {
     _createClass(Chef, [{
         key: 'cook',
         value: function cook() {
-            //方法,相当于function cook(){}
             console.log(this.food);
         }
     }, {
@@ -38,18 +32,15 @@ var Chef = function () {
             this.dish.push(dish);
         }
     }]);
-
     return Chef;
 }();
 
 //实例化
 //var chef=new Chef('毛豆炒肉');
-
-
 var chef = new Chef('毛豆炒肉');
 chef.cook();
 chef.food = '炸鸡';
-chef.menu = '炸虾球'; //.menu比.food更复杂
+chef.menu = '炸虾球'; 
 chef.menu = '披萨';
-//chef.dish.push('肯德基');      使用menu会变得更私有
+//chef.dish.push('肯德基');     
 console.log(chef.menu);
